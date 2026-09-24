355078697775ee4d20a9a48ff2c18364c0be8fa8
bereg.levente <bereg.levente@C116-02>
2026-09-24 12:44:23 +0200

    First part done

--- ConsoleApp1/MenuItem.cs
+++ ConsoleApp1/MenuItem.cs
@@ -12,7 +12,26 @@ namespace ConsoleApp1
         public string Category { get; set; }
         private int _price;
         public int Price { get { return _price; } set { if (value < 0) { value = 0; }; } }
-        public bool IsAvailable { get; set; }
-
+        public bool IsAvailable = true;
+        public static int Count = 0;
+        public void SellOut()
+        {
+            IsAvailable = false;
+        }
+        public void Restock()
+        {
+            IsAvailable = true;
+        }
+        public MenuItem(string name, string category, int price)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+            Count++;
+        }
+        public string GetDescription()
+        {
+            return $"Name: {Name} | Category: {Category} | Price: {Price}";
+        }
     }
 }
