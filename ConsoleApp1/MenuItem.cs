ad9084ff9f74f07846af5de2cb4e81babe5adc65
bereg.levente <bereg.levente@C116-02>
2026-09-24 12:10:09 +0200

    Added MenuItem.cs again...

---
+++ ConsoleApp1/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MenuItem
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        private int _price;
+        public int Price { get { return _price; } set { if (value < 0) { value = 0; }; } }
+        public bool IsAvailable { get; set; }
+
+    }
+}
